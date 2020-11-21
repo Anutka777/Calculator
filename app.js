0271db903d34cf14700a2b2911f9752ad93f9b4a
Anutka777 <rudak0wa.nat@yandex.ru>
2020-11-21 08:05:00 +0500

    Add operate()

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-let add = function(a, b) {
+const add = function(a, b) {
   return a + b;
 }
 
-let substruct = function(a, b) {
+const substruct = function(a, b) {
   return a - b;
 }
 
-let multiply = function(a, b) {
+const multiply = function(a, b) {
   return a * b;
 }
 
-let divide = function(a, b) {
+const divide = function(a, b) {
   return a / b;
+}
+
+const operate = function(operator, a, b) {
+  if (operator === 'add') {
+    return add(a, b);
+  } else if (operator === 'substruct') {
+    return substruct(a, b);
+  } else if (operator === 'multiply') {
+    return multiply(a, b);
+  } else if (operator === 'divide') {
+    return divide(a, b);
+  } else return "ERROR";
 }
