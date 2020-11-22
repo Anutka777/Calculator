34c0deb104c6ec2295e25f90f827211b5e394dae
Anutka777 <rudak0wa.nat@yandex.ru>
2020-11-22 17:34:20 +0500

    Add evaluation functions. Alfa's ready

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+let displayValue = '';
+let a;
+let b;
+let operator;
+
 const add = function(a, b) {
   return a + b;
 }
@@ -15,31 +20,47 @@ const divide = function(a, b) {
 }
 
 const operate = function(operator, a, b) {
-  if (operator === 'add') {
+  if (operator === '+') {
     return add(a, b);
-  } else if (operator === 'substruct') {
+  } else if (operator === '-') {
     return substruct(a, b);
-  } else if (operator === 'multiply') {
+  } else if (operator === '*') {
     return multiply(a, b);
-  } else if (operator === 'divide') {
+  } else if (operator === '/') {
     return divide(a, b);
   } else return "ERROR";
 }
 
-const buttons = Array.from(document.getElementsByClassName('digit'));
+let storeOperator = function(item) {
+  a = +displayValue;
+  operator = item.target.innerText;
+  displayValue = '';
+}
 
-// DEL
-// const getValue = function(item) {
-//   console.log(item.target.innerText);
-// };
+const showingResults = function() {
+  b = +displayValue;
+  displayValue = operate(operator, a, b);
+  document.getElementById('display').innerText = displayValue;
+  displayValue = '';
+}
 
 const fillDisplay = function(item) {
-  let displayValue = '';
   displayValue += item.target.innerText;
-  console.log(displayValue);
   document.getElementById('display').innerText = displayValue;
 }
 
-buttons.forEach(item => {
+const digitButtons = Array.from(document.getElementsByClassName('digit'));
+
+digitButtons.forEach(item => {
   item.addEventListener('click', fillDisplay);
 });
+
+const operatorButtons = Array.from(document.getElementsByClassName('operator'));
+
+operatorButtons.forEach(item => {
+  item.addEventListener('click', storeOperator);
+});
+
+const equalsButton = document.getElementById('equals');
+
+equalsButton.addEventListener('click', showingResults);
